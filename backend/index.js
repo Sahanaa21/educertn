4263f6933aba8e9a298624b3c867af024d7b0024
Sahana <sahanaa2060@gmail.com>
2026-04-20 00:18:25 +0530

    fix: load source entrypoint in backend development

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,8 @@
-const { startServer } = require('./dist/index.js');
-
-startServer();
+if (process.env.NODE_ENV === 'development') {
+	require('tsx/cjs');
+	const { startServer } = require('./src/index.ts');
+	startServer();
+} else {
+	const { startServer } = require('./dist/index.js');
+	startServer();
+}
